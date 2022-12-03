ecf2f4a82403972cbbb4b306cfe0a9bc5485e4b8
Celso <celsolarussa@gmail.com>
2022-12-03 14:26:33 -0300

    Alterando o método da classe Crawler

--- term/__main__.py
+++ term/__main__.py
@@ -30,7 +30,8 @@ class Crawler:
     def send_letters_in_cells(self, cells: list, word: List) -> None:
         [cell.send_keys(letter) for cell, letter in zip(cells, word)]
 
-    def check_win(self, attributes: List[Cell]) -> bool:
+    @classmethod
+    def check_win(cls, attributes: List[Cell]) -> bool:
         for attribute in attributes:
             if 'correta' not in attribute.result:
                 return False
